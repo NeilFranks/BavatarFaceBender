5e265b580343a9763b262aa84af90d9b0de61645
Neil <neilmichaelfranks@gmail.com>
2020-05-25 00:57:45 -0700

    volume controls color

--- Video.py
+++ Video.py
@@ -4,6 +4,8 @@ import math
 import socket
 import time
 
+from Audio import Audio
+
 
 class Video(object):
     def __init__(self):
@@ -12,11 +14,15 @@ class Video(object):
         self.took_prev = False
 
         # SETTINGS
-        self.BLEND = 0.5  # how much to blend frame to frame
+        self.blend_on = False  # whether or not to blend frame to frame
+        self.BLEND = 0.6  # how much to blend frame to frame
         self.BLUR_HOR = 1  # how much to blur the pixels together
         self.BLUR_VERT = 1  # how much to blur the pixels together
 
     def run(self):
+        audio = Audio(2)
+        audio.start()
+
         # Open Camera
         try:
             default = 0  # Try Changing it to 1 if webcam not found
@@ -30,17 +36,8 @@ class Video(object):
             _, frame = capture.read()
             ty = np.zeros(frame.shape, np.uint8)
 
-            # if self.took_prev:
-            #     blended_frame = cv2.addWeighted(
-            #         self.previous_frame, self.BLEND, frame, 1 - self.BLEND, 0
-            #     )
-            # else:
-            #     blended_frame = frame
-
-            blended_frame = frame
-
-            # self.previous_frame = blended_frame
-            # self.took_prev = True
+            # blend frame-to-frame (motion blur?)
+            blended_frame = self.blend_frames(frame)
 
             # Apply Gaussian blur
             blur = cv2.GaussianBlur(blended_frame, (1, 1), 0)
@@ -48,8 +45,15 @@ class Video(object):
             # Change color-space from BGR -> HSV
             rgb = cv2.cvtColor(blur, cv2.COLOR_BGR2RGB)
 
+            vol = audio.get_volume()
+
             self.drawPolygons(
-                rgb, ty, np.array([0, 0, 0]), np.array([10, 10, 10]), (0, 127, 255), 5
+                rgb,
+                ty,
+                np.array([0, 0, 0]),
+                np.array([80, 80, 80]),
+                (0, vol*127, vol*255),
+                5
             )
 
             cv2.imshow("JUICE", ty)
@@ -57,6 +61,8 @@ class Video(object):
 
             # Close the camera if 'q' is pressed
             if cv2.waitKey(1) == ord("q"):
+                audio.stop = True
+                audio.join()
                 break
 
         capture.release()
@@ -75,7 +81,8 @@ class Video(object):
         erosion = cv2.erode(dilation, kernel, iterations=1)
 
         # Apply Gaussian Blur and Threshold
-        filtered = cv2.GaussianBlur(erosion, (self.BLUR_HOR, self.BLUR_VERT), 0)
+        filtered = cv2.GaussianBlur(
+            erosion, (self.BLUR_HOR, self.BLUR_VERT), 0)
         _, thresh = cv2.threshold(filtered, 255, 255, 255)
 
         # Draw masks
@@ -86,6 +93,19 @@ class Video(object):
         for contour in contours:
             poly = cv2.approxPolyDP(contour, epsilon, True)
             cv2.drawContours(canvas, [poly], 0, color, cv2.FILLED)
+
+    def blend_frames(self, frame):
+        blended_frame = frame
+        if self.blend_on:
+            if self.took_prev:
+                blended_frame = cv2.addWeighted(
+                    self.previous_frame, self.BLEND, frame, 1 - self.BLEND, 0
+                )
+            else:
+                self.took_prev = True
+
+            self.previous_frame = blended_frame
+        return blended_frame
 
 
 Video().run()
